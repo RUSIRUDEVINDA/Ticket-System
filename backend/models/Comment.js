7dbd9bec064eff56da22528d6be588d8f80cb14c
R D <rusirud49@gmail.com>
2026-02-03 11:30:06 +0530

    feat : add Comment model

---
+++ backend/models/Comment.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    ticketId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ticket",
+        required: true
+    },
+
+    message: {
+        type: String,
+        required: true
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+},
+    { timestamps: true }
+
+)
+
+export default mongoose.model("Comment", commentSchema);
